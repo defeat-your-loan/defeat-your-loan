7ddebeb510aba342111de731657f1f210ecf8346
Kevin Holland <kevinholland94@gmail.com>
2021-04-29 00:04:55 -0400

    Styling changes

--- src/Calculator.svelte
+++ src/Calculator.svelte
@@ -20,76 +20,78 @@
 </script>
 
 <form on:submit={submit}>
-  <label for="startMonth">My loan started in</label>
-  <div>
-    <input
-      type="month"
-      placeholder="Loan Start Date"
-      id="startMonth"
-      autocomplete="off"
-      bind:value={$startMonth} />
-  </div>
-  <label for="amount">with the amount</label>
-  <div class="dollars">
-    <input
-      type="text"
-      placeholder="Loan Amount"
-      id="amount"
-      autocomplete="off"
-      bind:value={$amount} />
-  </div>
-  <label for="rate">at the interest rate</label>
-  <div class="rate">
-    <input
-      type="text"
-      placeholder="Interest Rate"
-      id="rate"
-      autocomplete="off"
-      bind:value={$rate} />
-  </div>
-  <label for="term">and has the term:</label>
-  <div class="row">
-    <div class="column">
+  <fieldset>
+    <label for="startMonth">My loan started in</label>
+    <div>
+      <input
+        type="month"
+        placeholder="Loan Start Date"
+        id="startMonth"
+        autocomplete="off"
+        bind:value={$startMonth} />
+    </div>
+    <label for="amount">with the amount</label>
+    <div class="dollars">
       <input
         type="text"
-        placeholder="Months/Years"
-        id="term"
+        placeholder="Loan Amount"
+        id="amount"
         autocomplete="off"
-        bind:value={$term} />
+        bind:value={$amount} />
     </div>
-    <div class="column column-25">
-      <select id="term-unit" autocomplete="off" bind:value={$termType}>
-        <option>Years</option>
-        <option>Months</option>
-      </select>
+    <label for="rate">at the interest rate</label>
+    <div class="rate">
+      <input
+        type="text"
+        placeholder="Interest Rate"
+        id="rate"
+        autocomplete="off"
+        bind:value={$rate} />
     </div>
-  </div>
-  {#if $loanStats}
-    <div class="stats">
-      <h3>
-        You pay <strong>{formatter.format($loanStats.monthlyPayment)}</strong> per month
-      </h3>
-      <h3 class="positive-text">
-        You have paid <strong>{formatter.format($loanStats.interestPaid)}</strong> in interest so far.
-      </h3>
-      <h3 class="negative-text">
-        You will pay <strong>{formatter.format($loanStats.interestToPay)}</strong> more interest for
-        the rest of the loan.
-      </h3>
-      <!-- <h3>Total Interest: ${stats.interestTotal}</h3> -->
+    <label for="term">and has the term:</label>
+    <div class="row">
+      <div class="column">
+        <input
+          type="text"
+          placeholder="Months/Years"
+          id="term"
+          autocomplete="off"
+          bind:value={$term} />
+      </div>
+      <div class="column column-25">
+        <select id="term-unit" autocomplete="off" bind:value={$termType}>
+          <option>Years</option>
+          <option>Months</option>
+        </select>
+      </div>
     </div>
+    {#if $loanStats}
+      <div class="stats">
+        <h3>
+          You pay <strong>{formatter.format($loanStats.monthlyPayment)}</strong> per month
+        </h3>
+        <h3 class="positive-text">
+          You have paid <strong>{formatter.format($loanStats.interestPaid)}</strong> in interest so far.
+        </h3>
+        <h3 class="negative-text">
+          You will pay <strong>{formatter.format($loanStats.interestToPay)}</strong> more interest for
+          the rest of the loan.
+        </h3>
+        <!-- <h3>Total Interest: ${stats.interestTotal}</h3> -->
+      </div>
 
-    <div class="mt-2">
-      <h3>"I want to"</h3>
-      <button on:click={setStrategy('payOffByDate')}>pay off by a date</button>
-      <button on:click={setStrategy('saveMoney')}>save money on my loan</button>
-    </div>
+      <div class="mt-2">
+        <h3>"I want to"</h3>
+        <button on:click={setStrategy('payOffByDate')}>pay off by a date</button>
+        <button on:click={setStrategy('saveMoney')}>save money on my loan</button>
+      </div>
 
-    {#if $strategy === 'payOffByDate'}
-      <PayOffBy />
+      {#if $strategy === 'payOffByDate'}
+        <PayOffBy />
+      {/if}
+      {#if $strategy === 'saveMoney'}
+        <SaveMoney />
+      {/if}
     {/if}
-    {#if $strategy === 'saveMoney'}
-      <SaveMoney />
-    {/if}
-  {/if}
+  </fieldset>
 </form>
